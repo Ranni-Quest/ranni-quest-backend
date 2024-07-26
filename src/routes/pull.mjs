454e60545c881b40252f94b123f2e5729c5ed82a
Emhisy <49534860+Emhisy@users.noreply.github.com>
2024-07-26 15:32:58 +0200

    change set

--- src/routes/pull.mjs
+++ src/routes/pull.mjs
@@ -2,7 +2,7 @@ import { serverConfig } from '../../../config/config.mjs';
 import { CheckAccess } from '../access_manager/check_access.mjs';
 import { dbConnect } from '../app.mjs';
 import { scarlet_violet } from '../data/drop.mjs';
-import { sv1Cards as cardsSet } from '../data/index.mjs';
+import { sv5Cards as cardsSet } from '../data/index.mjs';
 import { Hash } from '../util/hash.mjs';
 
 export class Pull {
